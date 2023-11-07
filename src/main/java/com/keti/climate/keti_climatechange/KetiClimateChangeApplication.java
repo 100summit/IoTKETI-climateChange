fbd4f188785155ad4591f79dcf81b4c28f2b61ca
DESKTOP-09T6H1A\SUMMIT <julkyu@summitcorp.co.kr>
2023-11-07 15:02:04 +0900

    최초커밋

---
+++ src/main/java/com/keti/climate/keti_climatechange/KetiClimateChangeApplication.java
@@ -0,0 +1,13 @@
+package com.keti.climate.keti_climatechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetiClimateChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetiClimateChangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keti/climate/keti_climatechange/KetiClimateChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keti.climate.keti_climatechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetiClimateChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
